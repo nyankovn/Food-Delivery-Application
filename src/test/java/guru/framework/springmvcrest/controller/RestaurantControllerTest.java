a27c4f75f78ccd9d0152cd4a39aa34d15cb2913e
Yankov,Nikola N <n.yankov@student.fontys.nl>
2020-11-15 12:02:22 +0100

    initial commit

--- src/test/java/guru/framework/springmvcrest/controller/RestaurantControllerTest.java
+++ src/test/java/guru/framework/springmvcrest/controller/RestaurantControllerTest.java
@@ -1,24 +1,24 @@
-package guru.framework.springmvcrest.controller;
-
-import guru.framework.springmvcrest.model.Restaurant;
-import guru.framework.springmvcrest.repository.RestaurantRepository;
-import guru.framework.springmvcrest.services.RestaurantService;
-import guru.framework.springmvcrest.services.RestaurantServiceImpl;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class RestaurantControllerTest {
-
-    @Mock
-    private RestaurantRepository restaurantRepository;
-
-    @InjectMocks
-    private RestaurantService restaurantService = new RestaurantServiceImpl(restaurantRepository);
+//package guru.framework.springmvcrest.controller;
+//
+//import guru.framework.springmvcrest.model.Restaurant;
+//import guru.framework.springmvcrest.repository.RestaurantRepository;
+//import guru.framework.springmvcrest.services.RestaurantService;
+//import guru.framework.springmvcrest.services.RestaurantServiceImpl;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class RestaurantControllerTest {
+//
+//    @Mock
+//    private RestaurantRepository restaurantRepository;
+//
+//    @InjectMocks
+//    private RestaurantService restaurantService = new RestaurantServiceImpl(restaurantRepository);
 
 //    @Test
 //    public void givenMockingIsDoneByMockito_whenGetIsCalled_shouldReturnMockedObject() {
@@ -31,4 +31,4 @@ public class RestaurantControllerTest {
 //        Employee employee = empService.getEmployee(id);
 //        Assert.assertEquals(emp, employee);
 //    }
-}
+//}
